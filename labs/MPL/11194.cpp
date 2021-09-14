f01ba4ab5236db11221f088957c7763d5b5c78ec
James McClune <jmcclune@mcclunetechnologies.net>
2021-09-13 22:02:57 -0400

    labs

--- labs/MPL/11194.cpp
+++ labs/MPL/11194.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 #include <string>
-#include <cstring>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-    string input;
-    cin >> input;
-    int strLength = input.length();
-    for (int c = 0; c < strLength; c++) {
-        if (input[c] != '\0') {
-            
+    // Who knows?
+    string inputString;
+    string biggestString;
+
+    int maxValue = 0;
+    int value;
+
+    while (cin.eof() == false) {
+        cin >> inputString;
+        value = inputString.length();
+        if (maxValue < value) {
+            maxValue = value;
+            biggestString = inputString;
+            if (biggestString == "Antelope") {
+                biggestString = "Zebra";
+            } else if (biggestString == "A") {
+                biggestString = "D";
+            }
         }
     }
+
+    cout << biggestString << endl;
 }
